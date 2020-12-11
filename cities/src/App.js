fc824dcd0209e090ca8d68b302f3e5d480e8ec4f
Vivadis <vivadi23@mail.ru>
2020-12-11 12:26:06 +0300

    Getting weather data

--- cities/src/App.js
+++ cities/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "11cfdcaf555401fad6a40bb44ed31f13"
+
+class App extends React.Component {
+    gettingWeather = async () => {
+        const api_url = await
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}&units=metric`);
+        const data = await api_url.json();
+        console.log(data);
+    }
+}
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Info />
+                <Form />
+                <Weather />
             </div>
         );
     }
